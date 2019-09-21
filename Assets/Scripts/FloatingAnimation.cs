64b6d61888d01690b7a3cf53229db091968b36a0
Steven Dijcks <steven.dijcks@kolibrigames.com>
2019-09-22 00:34:44 +0200

    Adds fire particle system

---
+++ Assets/Scripts/FloatingAnimation.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class FloatingAnimation : MonoBehaviour
+{
+    private Vector3 _startPosition;
+    private Vector3 _floatingPosition;
+    
+    [SerializeField] private float _horizontalSpeed;
+    [SerializeField] private float _horizontalDistance;
+    [SerializeField] private float _verticalSpeed;
+    [SerializeField] private float _verticalDistance;
+
+    private void Start()
+    {
+        _startPosition = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        _floatingPosition.x = Mathf.Sin(_horizontalSpeed * Time.time) * _horizontalDistance;
+        _floatingPosition.y = Mathf.Sin(_verticalSpeed * Time.time) * _verticalDistance;
+
+        transform.position = _startPosition + _floatingPosition;
+    }
+}
