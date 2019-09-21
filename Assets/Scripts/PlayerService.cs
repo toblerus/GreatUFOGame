13eeba0639d69e473899d00cc5106e616ee52dd4
Marcel Usslar <marcel.usslar@kolibrigames.com>
2019-09-21 19:24:08 +0200

    Added PlayerService.

---
+++ Assets/Scripts/PlayerService.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class PlayerService : MonoBehaviour
+{
+    public static PlayerService Instance { get; private set; }
+
+    private PlayerController[] _players;
+
+    private PlayerController ClosestPlayer(Vector2 currentPosition)
+    {
+        return _players.OrderBy(controller => Vector2.Distance(currentPosition, controller.transform.position)).First();
+    }
+
+    private float DistanceToClosestPlayer(Vector2 currentPosition)
+    {
+        return _players.Min(controller => Vector2.Distance(currentPosition, controller.transform.position));
+    }
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+
+        _players = FindObjectsOfType<PlayerController>();
+        if (_players.Length == 0)
+            Debug.LogError("No players were found in the scene.");
+    }
+}
