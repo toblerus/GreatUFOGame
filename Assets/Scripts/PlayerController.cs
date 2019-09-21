21208cd8711e28aeaf200453983aeceba6493679
Tobi <toblerus@hotmail.de>
2019-09-21 15:48:41 +0200

    PlayerController is working now, yay

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,21 +5,61 @@ using DG.Tweening;
 
 public class PlayerController : MonoBehaviour
 {
-    
+    [Header("References")]
+    [SerializeField] private GameObject projectile;
+    [SerializeField] private GameObject playerArm;
+    [SerializeField] private GameObject playerArmTip;
+    public Transform ufoPosition;
+
+    [Header("Player")]
     [SerializeField] private float movementSpeed;
+    [SerializeField] private float shootDelay = 0.1f;
+    [SerializeField] private float projectileSpeed = 5;
+    private float time = 0;
+    private bool exit;
 
     void Start()
     {
-        
+
     }
 
 
-    void Update()
+    void FixedUpdate()
     {
-        float y = Input.GetAxis("Vertical") * movementSpeed;
-        float x = Input.GetAxis("Horizontal") * movementSpeed;
-        x *= Time.deltaTime;
-        y *= Time.deltaTime;
-        transform.Translate(x, y, 0);
+        Move();
+        Shoot();
+        ArmFaceUFO();
+    }
+
+    private void Move()
+    {
+            float y = Input.GetAxisRaw("Vertical") * movementSpeed;
+            float x = Input.GetAxisRaw("Horizontal") * movementSpeed;
+            x *= Time.deltaTime;
+            y *= Time.deltaTime;
+            //x = Mathf.Clamp(x,  1, 10);
+            transform.Translate(x, y, 0);
+    }
+
+    private void Shoot()
+    {
+        if ((Input.GetButton("Fire1")) && (time > shootDelay))
+        {
+            time = 0;
+            var instantiatedProjectile = Instantiate(projectile, playerArmTip.transform.position, Quaternion.identity);
+            var projectileScript = instantiatedProjectile.GetComponent<Projectile>();
+            projectileScript.UFOPosition = ufoPosition;
+            projectileScript.projectileSpeed = projectileSpeed;
+        }
+        time += Time.deltaTime;
+    }
+
+    private void ArmFaceUFO()
+    {
+        Vector3 diff = ufoPosition.transform.position - transform.position;
+        diff.Normalize();
+
+       float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        playerArm.transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
     }
 }
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    public Transform UFOPosition;
+    public float projectileSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector3 diff = UFOPosition.transform.position - transform.position;
+        diff.Normalize();
+
+        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        transform.Translate(Vector2.up * projectileSpeed * Time.deltaTime);
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
     }
 }
