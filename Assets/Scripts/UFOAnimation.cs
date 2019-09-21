f8f23ee4ea8ad33c679260e923f47a8287089d49
Tobi <toblerus@hotmail.de>
2019-09-21 23:29:06 +0200

    UFO is slightly moving, yay!

---
+++ Assets/Scripts/UFOAnimation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFOAnimation : MonoBehaviour
+{
+    private Vector3 _startPosition;
+    public float speedUpDown = 1;
+    public float distanceUpDown = 1;
+
+    void Start()
+    {
+        _startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = _startPosition + new Vector3(0.0f, Mathf.Sin(speedUpDown * Time.time) * distanceUpDown, 0.0f);
+    }
+}
