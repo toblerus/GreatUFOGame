c81acfea518db7925f0b5a04e623d90c6a5d6af5
Steven Dijcks <steven.dijcks@kolibrigames.com>
2019-09-22 14:07:10 +0200

    Adds turret prefab and instantiates them on ufo

---
+++ Assets/Scripts/TurretController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class TurretController : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer _turretSprite;
+    [SerializeField] private Transform _bulletSpawnPoint;
+
+    public Transform BulletSpawnPoint => _bulletSpawnPoint;
+    
+    public void SetTurretSprite(Sprite sprite)
+    {
+        _turretSprite.sprite = sprite;
+    }
+}
